78f40b1e11d393ab7699c851364c7f1ca2a7eda2
Priyanshu Bindal <bindalpriyanshu6@gmail.com>
2025-03-23 15:47:11 +0530

    Order Matters in routing

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,13 +2,16 @@ const express = require('express');
 
 const app = express();
 
-
-app.use("/hello",(req,res)=>{
-    res.send("Its a Dashboard ! ")
-});
-
 app.use("/test",(req,res)=>{
     res.send("Test is Going on!")
+});
+
+app.use("/hello",(req,res)=>{
+    res.send("This is a Hello Page ! ")
+});
+
+app.use("/",(req,res)=>{
+    res.send("Its a Dashboard ! ")
 });
 
 app.listen(3001,()=>{
